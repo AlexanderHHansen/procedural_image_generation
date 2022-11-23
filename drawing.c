9c461ecd06473ef743792b5694f0f5c785063656
Alexander <ahha19@student.aau.dk>
2022-11-23 22:44:46 +0100

    image is not allocated dynamically

--- drawing.c
+++ drawing.c
@@ -11,9 +11,12 @@
 #include <time.h>
 #include <math.h>
 
-#define WIDTH 800
-#define HEIGHT 600 
+// Resolution of the image generated
+#define WIDTH 1200
+#define HEIGHT 1200
+
 typedef uint32_t Image[HEIGHT][WIDTH];
+typedef uint32_t** ImageDyn;
 typedef uint32_t color_t;
 
 //#define CENTROID_NUM (rand() % 40) + 5
@@ -52,13 +55,15 @@ color_t random_color() {
 /// @param arr 
 /// @param size 
 void generate_random_colors(color_t* arr, size_t size) {
-    for (size_t i = 0; i < size; i++)
-    {
+    for (size_t i = 0; i < size; i++) {
         arr[i] = random_color();
     }
 }
 
-void fill_image(Image img, color_t color) {
+/// @brief Fill the image with a color
+/// @param img Image to fill
+/// @param color Color to use
+void fill_image(ImageDyn img, color_t color) {
     for (size_t i = 0; i < HEIGHT; i++) {
         for (size_t j = 0; j < WIDTH; j++) {
             img[i][j] = color;
@@ -69,7 +74,7 @@ void fill_image(Image img, color_t color) {
 /// @brief Save image as .ppm file.
 /// @param img Image to save to file.
 /// @param filename path and filename. Must end with .ppm.
-void save_image(Image img, const char* filename) {
+void save_image(ImageDyn img, const char* filename) {
     FILE* fp = fopen(filename, "w+");
     if (fp==NULL) {
         printf("File could not be created");
@@ -91,16 +96,24 @@ void save_image(Image img, const char* filename) {
     fclose(fp);
 }
 
-void draw_square(Image img, uint32_t p_height, uint32_t p_width, color_t color, uint32_t square_width) {
-    assert(p_width <= WIDTH && p_height <= HEIGHT);
+/// @brief A shape_drawer functino. Will draw a square
+/// @param img Image to draw square on. 
+/// @param color The color every square will be 
+/// @param square_width width of each square.
+void draw_square(Image img, color_t color, uint32_t square_width) {
+    assert(WIDTH <= WIDTH && HEIGHT <= HEIGHT);
     unsigned int radius = square_width / 2;
-    for (size_t i = (p_height-radius) % HEIGHT; i <= (p_height+radius) % HEIGHT; i++) {
-        for (size_t j = (p_width-radius) % WIDTH; j <= (p_width+radius) % WIDTH; j++) {
+    for (size_t i = (HEIGHT-radius) % HEIGHT; i <= (HEIGHT+radius) % HEIGHT; i++) {
+        for (size_t j = (WIDTH-radius) % WIDTH; j <= (WIDTH+radius) % WIDTH; j++) {
             img[i][j] = color;
         }
     }
 }
 
+
+/// @brief Generate as many random points as size of input array 
+/// @param arr Array to store the points in 
+/// @param size Size of arr
 void generate_random_points(Point* arr, size_t size) {
     for (size_t i = 0; i < size; i++) {
         int ran_h = rand() % HEIGHT;
@@ -110,6 +123,11 @@ void generate_random_points(Point* arr, size_t size) {
     }
 }
 
+/// @brief Generate as many random pixels as size of input array.
+///        Each pixel will have a Random point and color. 
+///        Pixels will not exceed bounds of image. 
+/// @param arr Array to store the random pixels in 
+/// @param size Size of array
 void generate_random_pixels(Pixel* arr, size_t size) {
     Point* ran_points = malloc(sizeof(Point)*size);
     color_t* ran_colors = malloc(sizeof(color_t)*size);
@@ -132,6 +150,13 @@ void draw_ran_shapes(Image img, int num, int size, void (*shape_drawer)(Image, u
     free(points);
 }
 
+/// @brief Draw each pixel from array to image. Use shape_drawer function so pixel
+///        is large enought to see.
+/// @param img Image to draw the pixels on.
+/// @param arr Array of the pixels to be drawn.
+/// @param arr_size size of the pixel array.
+/// @param pixel_size How large each pixel should be drawn
+/// @param shape_drawer Function to draw the pixel onto image with.
 void draw_pixels(
     Image img, 
     Pixel* arr, 
@@ -149,6 +174,10 @@ void draw_pixels(
     }   
 }
 
+/// @brief Calculate the euclidian distance between two points. 
+/// @param p 
+/// @param q 
+/// @return The distance beetween point p and q.
 double euclidian_distance(Point p, Point q) {
     double p1 = (double)p.height;
     double p2 = (double)p.width;
@@ -157,6 +186,11 @@ double euclidian_distance(Point p, Point q) {
     return sqrt(pow(q1 - p1, 2) + pow(q2 - p2, 2));
 }
 
+/// @brief Find the cloest pixel in arr to the pixel_point
+/// @param arr Array of pixel centroids
+/// @param size Size of array 
+/// @param pixel_point Candidate pixel to find closest for
+/// @return The pixel closest to the pixel_point param
 Pixel find_closest_pixel(Pixel* arr, size_t size, Point pixel_point) {
     Pixel closest_pixel = arr[0];
     uint32_t prev_min_dist = euclidian_distance(arr[0].point, pixel_point);
@@ -175,7 +209,7 @@ Pixel find_closest_pixel(Pixel* arr, size_t size, Point pixel_point) {
 /// @param img image to cluster pixels on
 /// @param arr centroids
 /// @param size number of centroids
-void cluster_pixels(Image img, Pixel arr[], int size) {
+void cluster_pixels(ImageDyn img, Pixel arr[], int size) {
     for (uint32_t i = 0; i < HEIGHT; i++) {
         for (uint32_t j = 0; j < WIDTH; j++) {
             Point pixel_point = { .height=i, .width=j };
@@ -185,28 +219,55 @@ void cluster_pixels(Image img, Pixel arr[], int size) {
     }
 }
 
-void cluster_procedural_image_generation(Image img) {
+/// @brief Algorithm for procedurely generating an image. 
+///        Randomly select, a random number of pixels to be centroids.
+///        Give each centroid a random pixel. Cluster each pixel in image
+///        with the centroid and color each pixel with the closest centroid.        
+/// @param img Image to create the image with.
+void cluster_procedural_image_generation(ImageDyn img) {
     fill_image(img, random_color());
     int ran_pixel_count = CENTROID_NUM;//(rand() % 40) + 5;
     Pixel* pixel_arr = (Pixel*)malloc(sizeof(Pixel)*ran_pixel_count);
     generate_random_pixels(pixel_arr, ran_pixel_count);
     cluster_pixels(img, pixel_arr, ran_pixel_count);
-    // for (size_t i = 0; i < ran_pixel_count; i++)
-    // {
-    //     pixel_arr[i].color = COLOR_BLACK;
-    // }
-    // draw_pixels(img, pixel_arr, ran_pixel_count, 15, draw_square);
     free(pixel_arr);
 }
 
+/// @brief Once image exceeds 800*600, 2d array becomes too large
+///        for the stack. Therefore, allocate on heap using malloc.
+/// @return Pointer to dynamicly allocated 2d array 
+ImageDyn dynamic_image_arr() {
+    uint32_t** img = (uint32_t**)malloc(sizeof(uint32_t*)*HEIGHT);
+    for (uint32_t i = 0; i < HEIGHT; i++)
+        img[i] = (uint32_t*)malloc(sizeof(uint32_t)*WIDTH);
+    
+    return img;
+}
+
+/// @brief Free dynamicly created image
+/// @param img - Image to free 
+void free_image(ImageDyn img) {
+    for (uint32_t i = 0; i < HEIGHT; i++)
+    {
+        free(img[i]);
+    }
+    free(img);
+}
+
 int main() {
+    int seed = time(NULL);
+    srand(seed);
 
-    int seed = 1001;
-    srand(time(NULL));
+    ImageDyn img = dynamic_image_arr();
 
-    Image img;
     cluster_procedural_image_generation(img);
-    save_image(img, "output.ppm");
+
+    char filename[128];
+    sprintf(filename, "%d", seed);
+    strcat(filename, ".ppm");
+
+    save_image(img, filename);
+    free_image(img);
 
     return 0;
 }
